1eac0652fc6e6d099eacaacdf5657d504aca54b9
Abhijeetshavant <abhijeetshavant72@gmail.com>
2026-02-15 14:35:04 +0530

    frontend url added in cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://devyansh-decoration-frontend.vercel.app/",
     credentials: true,
   }),
 );
